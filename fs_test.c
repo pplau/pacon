9ea7fe212da43daf656fa91ac2634c13c9f096fc
pplau <pplauu@qq.com>
2019-04-12 23:41:01 -0700

    add fs test

---
+++ fs_test.c
@@ -0,0 +1,63 @@
+/*
+ *  written by Yubo
+ */
+
+#include <stdio.h>
+#include "pacon.h"
+
+int init_pacon()
+{
+	return 0;
+}
+
+int free_pacon()
+{
+	return 0;
+}
+
+int test_mkdir()
+{
+	return 0;
+}
+
+int test_create()
+{
+	return 0;
+}
+
+int test_readdir()
+{
+	return 0;
+}
+
+int test_open()
+{
+	return 0;
+}
+
+int test_rm()
+{
+	return 0;
+}
+
+int test_rm_dir()
+{
+	return 0;
+}
+
+int test_chmod()
+{
+	return 0;
+}
+
+int exc_test()
+{
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	int ret;
+
+	return 0;
+}
